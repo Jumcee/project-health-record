2d71421b977c9ce58c18940ab13ce9ae7e0395c3
Jumcee <futhmah456@gmail.com>
2023-11-18 22:55:34 +0100

    health record

--- contracts/HealthRecord.sol
+++ contracts/HealthRecord.sol
@@ -43,6 +43,7 @@ contract HealthRecord {
         _;
     }
 
+
     // Events for tracking record creation, updates, and transfers
     event RecordAdded(uint256 recordId, string ipfsHash);
     event RecordUpdated(uint256 recordId, string ipfsHash);
@@ -61,26 +62,92 @@ contract HealthRecord {
     }
 
     // Function to add a new medical record
-    function addMedicalRecord(string memory diagnosis, string memory treatmentPlan, string memory ipfsHash, uint256 patientId) public {
-        require(isDoctor(msg.sender), "Only doctors can add medical records");
-        require(isPatientRegistered(patientId), "Patient not registered");
-        medicalRecords[medicalRecords.length + 1] = MedicalRecord({
-            id: medicalRecords.length + 1,
-            diagnosis: diagnosis,
-            treatmentPlan: treatmentPlan,
-            ipfsHash: ipfsHash,
-            patientId: patientId,
-            doctorAddress: msg.sender
-        });
+    function addMedicalRecord(string memory diagnosis, string memory treatmentPlan, string memory ipfsHash, uint256 patientId) public isAuthorized(Role.Doctor) patientConsented(patientId, msg.sender) {
+    // Encrypt sensitive data here before storing it in the contract
+    string memory encryptedIpfsHash = encryptIPFSHash(ipfsHash);
 
-        emit RecordAdded(medicalRecords.length, ipfsHash);
+    medicalRecords[medicalRecords.length + 1] = MedicalRecord({
+        id: medicalRecords.length + 1,
+        diagnosis: diagnosis,
+        treatmentPlan: treatmentPlan,
+        ipfsHash: encryptedIpfsHash,
+        patientId: patientId,
+        doctorAddress: msg.sender
+    });
+
+    emit RecordAdded(medicalRecords.length, encryptedIpfsHash);
     }
+
 
     // Function to retrieve a specific medical record
     function getMedicalRecord(uint256 recordId) public view returns (MedicalRecord memory) {
-        require(medicalRecords[recordId].id > 0, "Record not found");
-        return medicalRecords[recordId];
+    require(medicalRecords[recordId].id > 0, "Record not found");
+    // Decrypt the IPFS hash before returning it
+    string memory decryptedIpfsHash = decryptIPFSHash(medicalRecords[recordId].ipfsHash);
+    return MedicalRecord({
+        id: medicalRecords[recordId].id,
+        diagnosis: medicalRecords[recordId].diagnosis,
+        treatmentPlan: medicalRecords[recordId].treatmentPlan,
+        ipfsHash: decryptedIpfsHash,
+        patientId: medicalRecords[recordId].patientId,
+        doctorAddress: medicalRecords[recordId].doctorAddress
+    });
     }
+
+    // Modifier to check if a user has a specific role
+    modifier hasRole(address user, Role requiredRole) {
+    require(doctors[user].role == requiredRole, "Unauthorized user");
+    _;
+    }
+
+    // Modifier to check if a doctor is associated with a patient's current hospital
+    modifier doctorAssociatedWithHospital(uint256 patientId) {
+    require(doctors[msg.sender].hospitalAddress == patients[patientId].currentHospitalAddress, "Doctor must be associated with patient's current hospital");
+    _;
+    }
+
+    // Function to give patient consent for a doctor
+function givePatientConsent(uint256 patientId, address doctorAddress) public {
+    patients[patientId].consent[doctorAddress] = true;
+}
+
+// Function to revoke patient consent for a doctor
+function revokePatientConsent(uint256 patientId, address doctorAddress) public {
+    patients[patientId].consent[doctorAddress] = false;
+}
+
+// Function to check if a patient has given consent to a doctor
+function checkPatientConsent(uint256 patientId, address doctorAddress) public view returns (bool) {
+    return patients[patientId].consent[doctorAddress];
+}
+
+// Function to log access to medical records
+function logAccessToMedicalRecord(uint256 recordId) private {
+    // Implement logging logic here
+}
+
+// Function to register a patient
+function registerPatient(uint256 id, string memory name, string memory dob, address hospitalAddress) public {
+    require(!isPatientRegistered(id), "Patient already registered");
+    patients[id] = Patient({id: id, name: name, dob: dob, currentHospitalAddress: hospitalAddress});
+}
+
+// Function to register a doctor
+function registerDoctor(string memory name, string memory specialization, address hospitalAddress) public {
+    require(!isDoctorRegistered(msg.sender), "Doctor already registered");
+    doctors[msg.sender] = Doctor({id: doctors.length + 1, name: name, specialization: specialization, hospitalAddress: hospitalAddress, role: Role.Doctor});
+}
+
+// Function to transfer a medical record to another hospital
+function transferRecord(uint256 recordId, address newHospitalAddress) public isAuthorized(Role.Doctor) doctorAssociatedWithHospital(medicalRecords[recordId].patientId) {
+    require(medicalRecords[recordId].id > 0, "Record not found");
+
+    address currentHospitalAddress = patients[medicalRecords[recordId].patientId].currentHospitalAddress;
+    patients[medicalRecords[recordId].patientId].currentHospitalAddress = newHospitalAddress;
+
+    emit RecordTransferred(recordId, currentHospitalAddress, newHospitalAddress);
+}
+
 
     // Function to update a medical record
     function updateMedicalRecord(uint256 recordId, string memory diagnosis, string memory treatmentPlan, string memory ipfsHash) public {
@@ -99,17 +166,6 @@ contract HealthRecord {
         emit RecordUpdated(recordId, ipfsHash);
     }
         
-    // Function to transfer a medical record to another hospital
-    function transferRecord(uint256 recordId, address newHospitalAddress) public {
-    require(isDoctor(msg.sender), "Only doctors can transfer records");
-    require(medicalRecords[recordId].id > 0, "Record not found");
-
-    address currentHospitalAddress = patients[medicalRecords[recordId].patientId].currentHospitalAddress;
-    require(currentHospitalAddress == msg.sender, "Doctor must be associated with the patient's current hospital to transfer the record");
-
-    patients[medicalRecords[recordId].patientId].currentHospitalAddress = newHospitalAddress;
-    emit RecordTransferred(recordId, currentHospitalAddress, newHospitalAddress);
-   }
 
     // Function to check if a patient is registered
     function isPatientRegistered(uint256 id) public view returns (bool) {
@@ -119,31 +175,5 @@ contract HealthRecord {
    // Function to check if a doctor is registered
    function isDoctorRegistered(address doctorAddress) public view returns (bool) {
     return doctors[doctorAddress].id > 0;
+   }
 }
-
-mapping(uint256 => bool) public patientConsent; // Mapping to track patient consent
-
-    modifier patientConsented(uint256 patientId) {
-        require(patientConsent[patientId], "Patient consent not given");
-        _;
-    }
-
-    modifier onlyDoctorOrAdmin() {
-        require(isDoctorRegistered(msg.sender) || isAdmin(msg.sender), "Unauthorized user");
-        _;
-    }
-
-    function addMedicalRecord(string memory diagnosis, string memory treatmentPlan, string memory ipfsHash, uint256 patientId) public onlyDoctorOrAdmin patientConsented(patientId) {
-        // Check consent and authorization before adding a medical record
-        // Implement encryption for sensitive data here or consider off-chain storage for IPFS hash
-    }
-
-    function givePatientConsent(uint256 patientId) public {
-        // Function for patients to give consent for accessing/managing their records
-        patientConsent[patientId] = true;
-    }
-
-    function isAdmin(address user) internal view returns (bool) {
-        // Function to check if the caller is an admin
-        // ...
-    }
